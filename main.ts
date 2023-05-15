891980949540d9e2e5e500f0b2bf9ccb9fede285
LennyBNF <132681048+LennyBNF@users.noreply.github.com>
2023-05-15 10:55:48 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+function base () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    strip.show()
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+strip.setBrightness(100)
+basic.forever(function () {
+    for (let index = 0; index <= 25; index++) {
+        strip.setBrightness(10 * index)
+        base()
+        basic.pause(100)
+    }
+})
